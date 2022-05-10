26b8bcfcb91f7a6d93bcacc2cc40b09ebc078394
Armando Aranda <arandaae19@gmail.com>
2022-05-10 08:41:28 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FactoryPatternExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the type of vehicle you would like.");
+            Console.WriteLine("Enter a 1 for a motorcycle.");
+            Console.WriteLine("Enter a 2 for a car");
+            
+            var vehicleType = Convert.ToInt32(Console.ReadLine());
+            IVehicle newVehicle = VehicleFactory.GetVehicle(vehicleType);
+
+            newVehicle.Drive();
+        }
+    }
+}
---
+++ VehicleFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int vehicleType)
+        {
+            return vehicleType switch
+            {
+                1 => new Motorcycle(),
+                2 => new Car(),
+                _ => new DefaultCar(),
+            };
+        }
+    }
+}
